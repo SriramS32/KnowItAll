d09a85062be04175d5d271d66e632543cc07ef64
Hemant Kunda <hkunda@usc.edu>
2017-10-15 20:52:26 -0700

    fixed bugs from test cases

--- controllers/entity.js
+++ controllers/entity.js
@@ -10,27 +10,46 @@ exports.insertEntity = function (entityData, user) {
         name: entityData.name,
         tags: entityData.tags
     });
-    entity.save((err) => {
+    entity.save((err, entity) => {
         if (err) console.log('error saving entity: ', err);
-        else exports.updateRating(entity._id, user, entityData.rating);
+        else {
+            exports.updateRating(entity._id, user, entityData.rating);
+        }
     });
     return entity._id;
 };
 
 exports.updateRating = function (entityId, user, rating) {
-    Entity.findOneAndUpdate({ id: entityId }, { $inc: { ratingTotal: rating, ratingCount: 1 } }, (err, entity) => {
-        if (err) console.log('error updating total rating: ', err);
-        else if (entity) {
-            const rating = new Rating({ user: user, rating: rating, entity: entityId });
-            rating.save((err) => {
-                if (err) console.log('error adding rating entry: ', err);
+    console.log('entity id: ' + entityId);
+    Rating.findOne({user: user}, (err, ratingEntry) => {
+        if (err) console.log('error checking for existing: ' + err);
+        else if (ratingEntry) {
+            let ratingDiff = rating - ratingEntry.rating;
+            ratingEntry.rating = rating;
+            ratingEntry.save((err) => {
+                if (err) console.log('error updating rating');
+            });
+            Entity.findOneAndUpdate({ _id: entityId }, { $inc: { ratingTotal: ratingDiff } }, (err, entity) => {
+                if (err) console.log('error updating total rating: ', err);
+            });
+        }
+        else {
+            Entity.findOneAndUpdate({ _id: entityId }, { $inc: { ratingTotal: rating, ratingCount: 1 } }, (err, entity) => {
+                if (err) console.log('error updating total rating: ', err);
+                else if (entity) {
+                    console.log('adding to rating table');
+                    const ratingEntry = new Rating({ user: user, rating: rating, entity: entityId });
+                    ratingEntry.save((err) => {
+                        if (err) console.log('error adding rating entry: ', err);
+                    });
+                }
             });
         }
     });
 };
 
 exports.fetchRating = function(entity) {
-    return Entity.findOne({id: entity}, 'ratingTotal ratingCount').exec();
+    return Entity.findOne({_id: entity}, 'ratingTotal ratingCount').exec();
 }
 
 exports.insertComment = function(commentData, entity) {
