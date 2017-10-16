649ce0e62fba7d27b1c943523f54b3b577aac372
Sriram Somasundaram <srirams32@gmail.com>
2017-10-15 19:31:43 -0700

    updating poll

--- controllers/poll.js
+++ controllers/poll.js
@@ -1,7 +1,6 @@
 const Schema = require('../models/Schema')
 const Poll = Schema.Poll
 const PollVote = Schema.PollVote
-mongoose.Promise = Promise;
 
 /**
  * Returns id of poll created
@@ -12,7 +11,8 @@ exports.insertPoll = function(pollData){
         options: pollData.options,
         owner: pollData.owner,
         createdOn: pollData.createdOn,
-        closedAfter: pollData.closedAfter
+        closedAfter: pollData.closedAfter,
+        tags: pollData.tags
     });
     poll.save((err) => {
         if (err) console.log("Error in creating poll, ", err);
@@ -35,79 +35,95 @@ exports.updatePollVotes = function(poll_ID, userID, userVote){
     });
 };
 
-function getPoll(poll_ID){
-    var promise = Poll.findOne({_id: poll_ID}).select('options').exec();
-    return promise;
+/**
+ * Returns a promise with the Poll
+ */
+exports.getPoll = function(poll_ID){
+    return Poll.findOne({_id: poll_ID}).exec();
 }
 
 /**
- * No need for updateVoteCount when displaying the polls.
- * Question and options below current chart displaying number for each poll choice.
- * Query poll vote collection for poll votes matching poll id and count choices on the spot.
+ * Returns a promise with PollVotes
  */
 exports.fetchPollCounts = function(poll_ID){
-    // pollOptions = [];
-    // var promise = getPoll(poll_ID)
-    //     .then(function (pollOpt){
-
-    //     })
-    //     .then(function (result){
-    //         pollCounts = []
-    //         return pollCounts
-    //     });
-
-    var promise = getPoll(poll_ID);
-    promise
-        .then(function (pollOpt){
-            pollCounts = new Array(pollOpt.length);
-            pollCounts.fill(0); // Initializing counts for each option to 0
-
-            PollVote.find({ poll: poll_ID }, function(err, voteDocs) {
-                if (err) console.log("Error while fetching poll counts, ", err);
-                if (! voteDocs){
-                    // Do something
-                    console.log("No poll counts for this id, ", poll_ID);
-                }
-                else{
-                    voteDocs.forEach(function(voteDoc) {
-                        pollCounts[voteDoc.choice] += 1
-                    });
-                }
-            });
-        }).error(function(error){
-            console.log(error);
-        });
-    // });
-    // return pollCounts;
-
-
-    // Poll.findOne({_id: poll_ID}, function(err, pollDoc) {
-    //     if (err) console.log("Error while fetching poll, ", err);
-    //     if (! pollDoc){
-    //         // Do something
-    //         console.log("No poll this id, ", poll_ID);
-    //     }
-    //     else{
-    //         pollOptions = pollDoc.options;
-    //     }
-    //     pollCounts = new Array(pollOptions.length);
-    //     pollCounts.fill(0); // Initializing counts for each option to 0
-    //     PollVote.find({poll: poll_ID}, function(err, voteDocs) {
-    //         if (err) console.log("Error while fetching poll counts, ", err);
-    //         if (! voteDocs){
-    //             // Do something
-    //             console.log("No poll counts for this id, ", poll_ID);
-    //         }
-    //         else{
-    //             voteDocs.forEach(function(voteDoc) {
-    //                 pollCounts[voteDoc.choice] += 1
-    //             });
-    //         }
-    //     });
-    //     return pollCounts;
-
-    // });
+    return PollVote.find({ poll: poll_ID }).exec();
 }
+
+// /**
+//  * No need for updateVoteCount when displaying the polls.
+//  * Question and options below current chart displaying number for each poll choice.
+//  * Query poll vote collection for poll votes matching poll id and count choices on the spot.
+//  * Returns Object:
+//  *      options: [String]
+//  *      counts:  [Number]
+//  */
+// exports.fetchPollCounts2 = function(poll_ID){
+//     // pollOptions = [];
+//     // var promise = getPoll(poll_ID)
+//     //     .then(function (pollOpt){
+
+//     //     })
+//     //     .then(function (result){
+//     //         pollCounts = []
+//     //         return pollCounts
+//     //     });
+
+//     let promise = getPoll(poll_ID);
+//     promise
+//         .then(function (pollOpt){
+//             pollCounts = new Array(pollOpt.length);
+//             pollCounts.fill(0); // Initializing counts for each option to 0
+
+//             PollVote.find({ poll: poll_ID }, function(err, voteDocs) {
+//                 if (err) console.log("Error while fetching poll counts, ", err);
+//                 if (! voteDocs){
+//                     // Do something
+//                     console.log("No poll counts for this id, ", poll_ID);
+//                 }
+//                 else{
+//                     voteDocs.forEach(function(voteDoc) {
+//                         pollCounts[voteDoc.choice] += 1
+//                     });
+//                 }
+//                 let retVal = new Object();
+//                 retVal.options = pollOpt;
+//                 retVal.counts = pollCounts
+//                 return retVal;
+//             });
+//         }).error(function(error){
+//             console.log(error);
+//         });
+//     // });
+//     // return pollCounts;
+
+
+//     // Poll.findOne({_id: poll_ID}, function(err, pollDoc) {
+//     //     if (err) console.log("Error while fetching poll, ", err);
+//     //     if (! pollDoc){
+//     //         // Do something
+//     //         console.log("No poll this id, ", poll_ID);
+//     //     }
+//     //     else{
+//     //         pollOptions = pollDoc.options;
+//     //     }
+//     //     pollCounts = new Array(pollOptions.length);
+//     //     pollCounts.fill(0); // Initializing counts for each option to 0
+//     //     PollVote.find({poll: poll_ID}, function(err, voteDocs) {
+//     //         if (err) console.log("Error while fetching poll counts, ", err);
+//     //         if (! voteDocs){
+//     //             // Do something
+//     //             console.log("No poll counts for this id, ", poll_ID);
+//     //         }
+//     //         else{
+//     //             voteDocs.forEach(function(voteDoc) {
+//     //                 pollCounts[voteDoc.choice] += 1
+//     //             });
+//     //         }
+//     //     });
+//     //     return pollCounts;
+
+//     // });
+// }
 
 /**
  * To be implemented further
