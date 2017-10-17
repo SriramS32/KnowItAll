5716daf939bc9dd3b28f4b9fa195ed7537051a25
Hemant Kunda <hkunda@usc.edu>
2017-10-16 18:47:05 -0700

    fixed entity route

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
 app.get('/', homeController.index);
 app.get('/landing', homeController.landing);
 app.get('/new-entry', homeController.newEntryPage);
-// app.get('/entity-page', homeController.entityPage);
+app.get('/entity/:entityId', homeController.entityPage);
 app.get('/escape-velocity', homeController.escapeVelocity);
 
 
